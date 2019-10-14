f5cfa052ad9ffb2fd7614460ebeaeaaf9ceb685a
Katie McBride <mcbride.katieb@gmail.com>
2019-10-14 15:55:45 -0500

    -m'bootstrap calendar made and beginning jQuery'

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+    // $( "#rowInfo" ).click(function() {
+    //     alert( "clicked" );
+    // });
+
+    $(document).on('click',"#rowInfo", function(){
+        alert( "clicked" );
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
