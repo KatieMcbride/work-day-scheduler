6fbd43457eaa3649d5e64a52783be523a50cfe3a
Katie McBride <mcbride.katieb@gmail.com>
2019-10-14 21:08:06 -0500

    -m'updated id's and java'

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
 
-    // $( "#rowInfo" ).click(function() {
-    //     alert( "clicked" );
-    // });
-
-    $(document).on('click',"#rowInfo", function(){
-        alert( "clicked" );
+    $("#save").click(function(){
+        textOne = document.getElementById("rowInfo1").value;
+        // console.log(textOne);
+        localStorage.setItem("rowInfo1", textOne);
+        
     });
 
 
-
+    // $("#")
+    // value = $("#rowInfo1").val();
+    // console.log(value);
 
 
 
