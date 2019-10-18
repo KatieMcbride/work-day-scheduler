ec796d37e85fc9412aaf63ac2cb045ce10c1b3c5
Katie McBride <mcbride.katieb@gmail.com>
2019-10-18 14:37:52 -0500

    finished indentation

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 $(document).ready(function(){
+  //  INIT
+  checkTime();
 
-    function checkTime() {
-        let currentDate = moment().format('MMM Do YYYY');
-          $('#txtDate').text(`Today is: ${currentDate}`);
-        let currentTime = moment().format('h:mm a');
-        $('#current-time').text(`It is: ${currentTime}`);
-        let currentHour = moment().hours();
-        console.log(currentHour);
-          let inputTime = $('input');
+  function checkTime() {
+    let currentDate = moment().format('MMM Do YYYY');
+    $('#txtDate').text(`Today is: ${currentDate}`);
+    let currentTime = moment().format('h:mm a');
+    $('#current-time').text(`It is: ${currentTime}`);
+    let currentHour = moment().hours();
+    console.log(currentHour);
+    let inputTime = $('input');
           
-          for (i=0; i < inputTime.length; i++) {
-            let idTime = inputTime[i].id 
-            idTime = parseInt(idTime.slice(0, -1));
-            console.log('id tags', inputTime[i].id);
-            console.log('idTime', idTime);
-            if (currentHour > idTime) {
-              $(`#${inputTime[i].id}`).attr('style', 'background-color:silver');
-            } else if (currentHour < idTime) {
-              $(`#${inputTime[i].id}`).attr('style', 'background-color: coral');
-            } else if (currentHour === idTime) {
-              $(`#${inputTime[i].id}`).attr('style', 'background-color: RosyBrown');
-            }
-          };
-        };
-  
-    
+    for (i=0; i < inputTime.length; i++) {
+      let idTime = inputTime[i].id 
+      idTime = parseInt(idTime.slice(0, -1));
+      console.log('id tags', inputTime[i].id);
+      console.log('idTime', idTime);
+      if (currentHour > idTime) {
+        $(`#${inputTime[i].id}`).attr('style', 'background-color:silver');
+          } else if (currentHour < idTime) {
+        $(`#${inputTime[i].id}`).attr('style', 'background-color: coral');
+          } else if (currentHour === idTime) {
+        $(`#${inputTime[i].id}`).attr('style', 'background-color: RosyBrown');
+      }
+    };
+  };
+ 
 
-    checkTime();
-    
-    
-    renderLastRegistered();
+  renderLastRegistered();
 
     function renderLastRegistered() {
     $("#saveInput1").val(localStorage.getItem("6AM"));
@@ -47,52 +45,6 @@ $(document).ready(function(){
     };
 
 });
-
-   
-    // Next figure out the colors
-//     function getStylesheet() {
-//         var currentTime = new Date().getHours();
-//         if (0 <= currentTime&&currentTime < 5) {
-//           // If time is after 7PM or before 6AM, apply night theme to ‘body’
-//         document.getElementById("sixAM").className = "row future";
-//         document.getElementById("sevenAM").className = "row future";
-//         document.getElementById("eightAM").className = "row future";
-//         document.getElementById("nineAM").className = "row future";
-//         document.getElementById("tenAM").className = "row future";
-//         document.getElementById("elevenAM").className = "row future";
-//         document.getElementById("twelvePM").className = "row future";
-//         document.getElementById("onePM").className = "row future";
-//         document.getElementById("twoPM").className = "row future";
-//         document.getElementById("threePM").className = "row future";
-//         document.getElementById("fourPM").className = "row future";
-//         document.getElementById("fivePM").className = "row future";
-//         }
-//         if (5 <= currentTime&&currentTime < 11) {
-//          document.write("<link rel='stylesheet' href='morning.css' type='text/css'>");
-//         }
-//         if (11 <= currentTime&&currentTime < 16) {
-//          document.write("<link rel='stylesheet' href='day.css' type='text/css'>");
-//         }
-//   };
-
-//   if (n > 19 || n < 6)
-//   // If time is after 7PM or before 6AM, apply night theme to ‘body’
-//   document.body.className = "night";
-// else if (n > 16 && n < 19)
-//   // If time is between 4PM – 7PM sunset theme to ‘body’
-//   document.body.className = "sunset";
-// else
-//   // Else use ‘day’ theme
-//   document.body.className = "day";
-  
-//   getStylesheet();
-
-
-    // responsive time to change the colors
-
-    // Update CSS
-
-    // DRY code
 
     
     
